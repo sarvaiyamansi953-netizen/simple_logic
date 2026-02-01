93c1a458a3c9fb584f6b5329388afc5a91929654
Mansi Sarvaiya <sarvaiyamansi953@gmail.com>
2026-02-01 14:52:48 +0530

    Add files via upload

---
+++ basic.py
@@ -0,0 +1,13 @@
+#Average and Maximum
+marks = [78, 85, 90, 88]
+avg = sum(marks) / len(marks)
+print("Average:", avg)
+
+data = [12, 45, 67, 23, 89]
+print("Maximum value:", max(data))
+
+#Function to calculate square
+def square(x):
+    return x * x
+
+print(square(5))
